473c0f8d6cbe208d7081ac4b83685a7ffbfec81d
harshbhar0629 <harshbhar.0629@gmail.com>
2024-04-03 10:57:11 +0530

    Add Files

--- web_dev/javaScript/Pass_generator/app.js
+++ web_dev/javaScript/Pass_generator/app.js
@@ -11,7 +11,7 @@ const upperCase = document.querySelector("#uppercase");
 const number = document.querySelector("#number");
 const symbol = document.querySelector("#symbol");
 const indicator = document.querySelector("[data-indicator]");
-const generateBtn = document.querySelector("[generate-password]");
+const generateBtn = document.querySelector(".generate-password");
 const allCheckBox = document.querySelector("input[type=checkbox");
 const symbols = "~`!@#$%^&*()_+-{[}]|:;/<>.,'";
 
@@ -49,33 +49,34 @@ function handleSlider() {
 
 // set indicator
 function setIndicator(color) {
-    indicator.computedStyleMap.backgroundColor = color;
+    indicator.style.backgroundColor = color;
     // shadow 
 }
 
 // get random values
-function getRandomVal(min, max) {
-    return Math.floor(Math.random()) * (max - min) + min;
-}
+// function getRandomVal(min, max) {
+//     return (Math.floor(Math.random()) * (max - min)) + min;
+// }
 
 //generate random int
 function generateRandomInt() {
-    return getRandomVal(0, 9);
+    return (Math.floor(Math.random() * (10))) % 10;
 }
 
 //generate lowercase character
 function generateRandomLowercase() {
-    return String.fromCharCode(getRandomVal(97, 97 + 26));
+    return String.fromCharCode(((Math.floor(Math.random() * (27))) % 26) + 97)
 }
 
 //generate uppercase character
 function generateRandomUppercase() {
-    return String.fromCharCode(getRandomVal(65, 65 + 26));
+    return String.fromCharCode(((Math.floor(Math.random() * (27))) % 26) + 65);
 }
 
 // generate symbols 
 function generateRandomSymbol() {
-    return symbols[getRandomVal(0, symbols.length) % symbols.length];
+    let idx = (Math.floor(Math.random() * symbols.length)) % symbols.length;
+    return symbols[idx];
 }
 
 //set color for strength
@@ -93,7 +94,7 @@ function setColorStrength() {
         // strong pass 
         setIndicator("#0f0");
     }
-    else if ((hasSym == true || hasNum == true) && (hasLower == true || hasUpper == true) && passLen >= 8) {
+    else if ((hasSym == true || hasNum == true) && (hasLower == true || hasUpper == true) && passLen >= 6) {
         // moderate
         setIndicator("#ff0");
     }
@@ -102,3 +103,118 @@ function setColorStrength() {
         setIndicator("#f00");
     }
 }
+
+async function copyContent() {
+    try {
+        await navigator.clipboard.writeText(passDisplay.value);
+        copyMsg.innerHTML = "Copied";
+    }catch(e) {
+        copyMsg.innerHTML = "Failed";
+    }
+    copyMsg.classList.add("active");
+    setTimeout(() => {
+        copyMsg.innerHTML = "";
+        copyMsg.classList.remove("active");
+    }, 1500);
+}
+
+inputSlider.addEventListener("input", (event) => {
+    passLen = event.target.value;
+    handleSlider();
+});
+
+copyBtn.addEventListener("click", () => {
+    if (password.length > 0 || passDisplay.value) {
+        copyContent();
+    }
+});
+
+// cnt check box
+upperCase.addEventListener('click', () => {
+    if (upperCase.checked) {
+        cntCheckBox++;
+    }
+    else {
+        cntCheckBox--;
+    }
+    // console.log(cntCheckBox);
+});
+
+lowerCase.addEventListener('click', () => {
+    if (lowerCase.checked) {
+        cntCheckBox++;
+    }
+    else {
+        cntCheckBox--;
+    }
+    // console.log(cntCheckBox);
+});
+
+number.addEventListener('click', () => {
+    if (number.checked) {
+        cntCheckBox++;
+    }
+    else {
+        cntCheckBox--;
+    }
+    // console.log(cntCheckBox);
+});
+
+symbol.addEventListener('click', () => {
+    if (symbol.checked) {
+        cntCheckBox++;
+    }
+    else {
+        cntCheckBox--;
+    }
+    // console.log(cntCheckBox);
+});
+
+function generatePass() {
+    password = "";
+    let len = passLen;
+    while (len > 0) {
+        len--;
+        let r = Math.floor(Math.random() * 4) ;
+        if (r == 1 && lowerCase.checked) {
+            password += generateRandomLowercase();
+        }
+        else if (r == 2 && upperCase.checked) {
+            password += generateRandomUppercase();
+        }
+        else if (r == 3 && number.checked) {
+            password += generateRandomInt();
+        }
+        else if (r == 4 && symbol.checked) {
+            password += generateRandomSymbol();
+        }
+        else {
+            if (symbol.checked) {
+                password += generateRandomSymbol();
+            }
+            else if (number.checked) {
+                password += generateRandomInt();
+            }
+            else if (upperCase.checked) {
+                password += generateRandomUppercase();
+            }
+            else if (lowerCase.checked) {
+                password += generateRandomLowercase();
+            }
+        }
+        console.log(password);
+    }
+}
+
+// main function 
+generateBtn.addEventListener('click', () => {
+    if (cntCheckBox > 0) {
+        generatePass();
+        passDisplay.value = password;
+        console.log(password);
+    }
+    else {
+        console.warn("Please select any checkbox for password");
+    }
+});
+
