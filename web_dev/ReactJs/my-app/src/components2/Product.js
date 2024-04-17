076bf0b3333c287be79df80411d9c608bcc398f4
harshbhar0629 <harshbhar.0629@gmail.com>
2024-04-17 13:42:43 +0530

    Handling event on New Product:

--- web_dev/ReactJs/my-app/src/components2/Product.js
+++ web_dev/ReactJs/my-app/src/components2/Product.js
@@ -1,16 +1,32 @@
 import './Product.css'
+import { useState } from 'react';
 
 function Product() {
+
+    let [title, setTitle] = useState('');
+    let [date, setDate] = useState('');
+
+    function newItemHandler(event) {
+        setTitle(event.target.value);
+        console.log(event.target.value);
+    }
+
+    function dateHandler(event) {
+        setDate(event.target.value);
+        console.log(event.target.value);
+        console.log(event)
+    }
+
     return (
         <form className='form'>
             <div className='title'>
                 <label>
                     Title
                 </label>
-                <input type="text" placeholder='Enter Product'></input>
+                <input onChange={newItemHandler} type="text" placeholder='Enter Product'></input>
             </div>
             <div className='divi'>
-                <div className='date'>
+                <div onChange={dateHandler} className='date'>
                     <label>Date</label>
                     <input type='date'></input>
                 </div>
