2e92295daf1e794d34ed3b3fd0b5c88ee6043f03
harshbhar0629 <harshbhar.0629@gmail.com>
2024-04-26 08:52:37 +0530

    Add complete code:

--- web_dev/ReactJs/Top-courses/src/App.js
+++ web_dev/ReactJs/Top-courses/src/App.js
@@ -1,36 +1,68 @@
 /** @format */
 
-import React, { useEffect, useState } from "react";
-import Navbar from "./component/Navbar";
-import Filter from "./component/Filter";
-import Cards from "./component/Cards";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Cards from "./components/Cards";
+import Filter from "./components/Filter";
 import { apiUrl, filterData } from "./data";
-// import {toast} from "react-toastify"
+import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
+import { toast } from "react-toastify";
 
 const App = () => {
+	const [courses, setCourses] = useState({});
+	const [loading, setLoading] = useState(true);
+	const [category, setCategory] = useState(filterData[0].title);
 
-	const [courses, setCourses] = useState("")
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await fetch(apiUrl);
-				const output = await res.json();
-				setCourses(output.data);
-				console.log(output.data);
-			}
-			catch (err) {
-				// toast.error("something went wrong");
-				console.error("Something went wrong");
-			}
+	async function fetchData() {
+		setLoading(true);
+		try {
+			let response = await fetch(apiUrl);
+			let output = await response.json();
+			///output ->
+			setCourses(output.data);
+		} catch (error) {
+			toast.error("Unable to fetch data form API");
 		}
+		setLoading(false);
+	}
+
+	useEffect(() => {
 		fetchData();
-	},[])
+	}, []);
 
 	return (
-		<div className="wrapper">
-			<Navbar></Navbar>
-			<Filter filterData={filterData}></Filter>
-			<Cards courses={courses}></Cards>
+		<div className="min-h-screen flex flex-col bg-bgDark2">
+			<div>
+				<Navbar />
+			</div>
+			<div className="bg-bgDark2">
+				<div>
+					<Filter
+						filterData={filterData}
+						category={category}
+						setCategory={setCategory}
+					/>
+				</div>
+				<div
+					className="w-11/12 max-w-[1200px] 
+        mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+					{courses.length === 0 || Object.keys(courses).length === 0 ? (
+						<div>No Courses Found</div>
+					) : loading ? (
+						<Spinner />
+					) : (
+						<Cards
+							courses={courses}
+							category={category}
+						/>
+					)}
+
+					{/* {
+            loading ? (<Spinner/>) : (<Cards courses={courses} category={category}/>)
+          } */}
+				</div>
+			</div>
 		</div>
 	);
 };
